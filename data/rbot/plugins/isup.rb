4c3a13e1d0f4e6244d10472593dd1797dffa88c5
Rob B <rb.cubed@gmail.com>
2011-10-27 23:04:33 -0400

    ratio and buffer in whatcd new plugin isup

---
+++ data/rbot/plugins/isup.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'mechanize'
+
+class IsupPlugin < Plugin
+
+  def help(plugin, topic)
+    "isup <site> => Check status of a website"
+  end
+
+  def isup(m, params)
+    site = params[:site]
+    agent = Mechanize.new
+    page = agent.get('http://www.isup.me/' + site)
+
+    page.root.xpath("//*[@id='container']").each do
+      |li|
+      if li.text =~ /It's just you. (.*)/
+        m.reply "It's up!"
+      else
+        m.reply "It's down!"
+      end
+    end
+  end
+end
+
+plugin = IsupPlugin.new
+plugin.map 'isup [:site]', :action => :isup, :thread => true
